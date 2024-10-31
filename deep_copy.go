aaff312807168be8af0a3135a1cad667c4c9303b
Tanner Kline <tanner.kline@intrusion.com>
2024-10-31 14:41:12 -0500

    adding deepcopyexported

--- deep_copy.go
+++ deep_copy.go
@@ -1,11 +1,8 @@
 package helpers
 
-// import (
-// 	"bytes"
-// 	"encoding/gob"
-// )
-
 import (
+	"bytes"
+	"encoding/gob"
 	"unsafe"
 )
 
@@ -31,20 +28,20 @@ func DeepCopy[T any](obj T) T {
 }
 
 // DeepCopy returns a deep copy of specified object
-// func DeepCopy[T any](obj T) T {
-// 	var buf bytes.Buffer
-// 	encoder := gob.NewEncoder(&buf)
-// 	if err := encoder.Encode(obj); err != nil {
-// 		var zero T
-// 		return zero
-// 	}
+func DeepCopyExported[T any](obj T) T {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(obj); err != nil {
+		var zero T
+		return zero
+	}
 
-// 	var copy T
-// 	decoder := gob.NewDecoder(&buf)
-// 	if err := decoder.Decode(&copy); err != nil {
-// 		var zero T
-// 		return zero
-// 	}
+	var copy T
+	decoder := gob.NewDecoder(&buf)
+	if err := decoder.Decode(&copy); err != nil {
+		var zero T
+		return zero
+	}
 
-// 	return copy
-// }
+	return copy
+}
