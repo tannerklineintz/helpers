a77981262ef24fdab5c4b8bb8fabf46916dea502
Tanner Kline <tanner.kline@intrusion.com>
2024-10-31 10:25:44 -0500

    logs

--- deep_copy.go
+++ deep_copy.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"bytes"
 	"encoding/gob"
+	"log"
 )
 
 // DeepCopy returns a deep copy of specified object
@@ -11,9 +12,11 @@ func DeepCopy(obj any) any {
 	var copy any
 	encoder, decoder := gob.NewEncoder(&buf), gob.NewDecoder(&buf)
 	if err := encoder.Encode(obj); err != nil {
+		log.Printf("%v", err.Error())
 		return nil
 	}
 	if err := decoder.Decode(copy); err != nil {
+		log.Printf("%v", err.Error())
 		return nil
 	}
 
