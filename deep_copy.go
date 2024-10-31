57e4fa910bc9f58f27b9bf5b0124b16f80269041
Tanner Kline <tanner.kline@intrusion.com>
2024-10-31 14:25:45 -0500

    hmmm

--- deep_copy.go
+++ deep_copy.go
@@ -13,35 +13,45 @@ import (
 // DeepCopy returns a deep copy of the specified object, including unexported fields.
 func DeepCopy[T any](obj T) T {
 	val := reflect.ValueOf(obj)
+
+	// Ensure the value is addressable
+	if val.Kind() != reflect.Ptr {
+		valPtr := reflect.New(val.Type())
+		valPtr.Elem().Set(val)
+		val = valPtr
+	}
+
 	copyVal := deepCopyValue(val)
 	return copyVal.Interface().(T)
 }
 
 func deepCopyValue(src reflect.Value) reflect.Value {
+	srcType := src.Type()
+
 	switch src.Kind() {
 	case reflect.Ptr:
 		if src.IsNil() {
-			return reflect.Zero(src.Type())
+			return reflect.Zero(srcType)
 		}
-		dstPtr := reflect.New(src.Type().Elem())
+		dstPtr := reflect.New(srcType.Elem())
 		dstPtr.Elem().Set(deepCopyValue(src.Elem()))
 		return dstPtr
 	case reflect.Interface:
 		if src.IsNil() {
-			return reflect.Zero(src.Type())
+			return reflect.Zero(srcType)
 		}
-		return deepCopyValue(src.Elem())
+		srcElem := src.Elem()
+		dstElem := deepCopyValue(srcElem)
+		return dstElem
 	case reflect.Struct:
-		dst := reflect.New(src.Type()).Elem()
+		dst := reflect.New(srcType).Elem()
 		for i := 0; i < src.NumField(); i++ {
 			srcField := src.Field(i)
 			dstField := dst.Field(i)
 
 			// Handle unexported fields
-			if !srcField.CanInterface() {
+			if !srcField.CanSet() {
 				srcField = reflect.NewAt(srcField.Type(), unsafe.Pointer(srcField.UnsafeAddr())).Elem()
-			}
-			if !dstField.CanSet() {
 				dstField = reflect.NewAt(dstField.Type(), unsafe.Pointer(dstField.UnsafeAddr())).Elem()
 			}
 
@@ -50,24 +60,24 @@ func deepCopyValue(src reflect.Value) reflect.Value {
 		return dst
 	case reflect.Slice:
 		if src.IsNil() {
-			return reflect.Zero(src.Type())
+			return reflect.Zero(srcType)
 		}
-		dst := reflect.MakeSlice(src.Type(), src.Len(), src.Cap())
+		dst := reflect.MakeSlice(srcType, src.Len(), src.Cap())
 		for i := 0; i < src.Len(); i++ {
 			dst.Index(i).Set(deepCopyValue(src.Index(i)))
 		}
 		return dst
 	case reflect.Map:
 		if src.IsNil() {
-			return reflect.Zero(src.Type())
+			return reflect.Zero(srcType)
 		}
-		dst := reflect.MakeMapWithSize(src.Type(), src.Len())
+		dst := reflect.MakeMapWithSize(srcType, src.Len())
 		for _, key := range src.MapKeys() {
 			dst.SetMapIndex(deepCopyValue(key), deepCopyValue(src.MapIndex(key)))
 		}
 		return dst
 	case reflect.Array:
-		dst := reflect.New(src.Type()).Elem()
+		dst := reflect.New(srcType).Elem()
 		for i := 0; i < src.Len(); i++ {
 			dst.Index(i).Set(deepCopyValue(src.Index(i)))
 		}
