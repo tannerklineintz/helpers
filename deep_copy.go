77f1339eef95865e3a6b477e946adb2b6627773a
Tanner Kline <tanner.kline@intrusion.com>
2024-10-31 15:02:20 -0500

    ok well i guess stephen is just the superior human

--- deep_copy.go
+++ deep_copy.go
@@ -1,24 +1,13 @@
 package helpers
 
 import (
-	"bytes"
-	"encoding/gob"
 	"reflect"
 )
 
+// DeepCopy returns a deep copy of the specified object, including unexported fields.
 func DeepCopy[T any](obj T) (T, error) {
-	var copy T
-	// Option 1: Using JSON marshaling (simpler but slower)
-	// data, err := json.Marshal(obj)
-	// if err != nil {
-	// 	return copy, err
-	// }
-	// err = json.Unmarshal(data, &copy)
-	// return copy, err
-	// Option 2: Using reflection (more complex but handles more cases)
 	copyVal, err := deepCopyReflect(obj)
-	copy = copyVal.(T)
-	return copy, err
+	return copyVal.(T), err
 }
 
 // Alternative reflection-based implementation
@@ -69,44 +58,4 @@ func copyRecursive(original, copy reflect.Value) {
 			copy.Set(original)
 		}
 	}
-}
-
-// DeepCopy returns a deep copy of the specified object, including unexported fields.
-// func DeepCopy[T any](obj T) T {
-// 	var copyVal T
-
-// 	// Get the size of the object in bytes
-// 	size := unsafe.Sizeof(obj)
-
-// 	// Get pointers to the source and destination objects
-// 	srcPtr := unsafe.Pointer(&obj)
-// 	dstPtr := unsafe.Pointer(&copyVal)
-
-// 	// Create byte slices pointing to the memory of the objects
-// 	srcSlice := unsafe.Slice((*byte)(srcPtr), size)
-// 	dstSlice := unsafe.Slice((*byte)(dstPtr), size)
-
-// 	// Copy the memory from the source to the destination
-// 	copy(dstSlice, srcSlice)
-
-// 	return copyVal
-// }
-
-// DeepCopy returns a deep copy of specified object with only the exported values
-func DeepCopyExported[T any](obj T) T {
-	var buf bytes.Buffer
-	encoder := gob.NewEncoder(&buf)
-	if err := encoder.Encode(obj); err != nil {
-		var zero T
-		return zero
-	}
-
-	var copy T
-	decoder := gob.NewDecoder(&buf)
-	if err := decoder.Decode(&copy); err != nil {
-		var zero T
-		return zero
-	}
-
-	return copy
 }
