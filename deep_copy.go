91739b13b13ef39bc70d21f50588db4096d299ba
Tanner Kline <tanner.kline@intrusion.com>
2024-10-31 10:57:12 -0500

    hmmm

--- deep_copy.go
+++ deep_copy.go
@@ -3,19 +3,29 @@ package helpers
 import (
 	"bytes"
 	"encoding/gob"
+	"reflect"
 )
 
 // DeepCopy returns a deep copy of specified object
 func DeepCopy(obj any) any {
 	var buf bytes.Buffer
-	var copy any
-	encoder, decoder := gob.NewEncoder(&buf), gob.NewDecoder(&buf)
+	encoder := gob.NewEncoder(&buf)
 	if err := encoder.Encode(obj); err != nil {
 		return nil
 	}
-	if err := decoder.Decode(&copy); err != nil {
+
+	// Create a new zero value of the same type
+	objType := reflect.TypeOf(obj)
+	copyValue := reflect.New(objType).Interface()
+
+	decoder := gob.NewDecoder(&buf)
+	if err := decoder.Decode(copyValue); err != nil {
 		return nil
 	}
 
-	return copy
+	// Dereference pointer if original object is not a pointer
+	if objType.Kind() != reflect.Ptr {
+		return reflect.ValueOf(copyValue).Elem().Interface()
+	}
+	return copyValue
 }
